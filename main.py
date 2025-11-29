3a88cf9d806d5c42bbd0b0c1e4c5e68bac0fed08
dharsann <131436118+dharsann@users.noreply.github.com>
2025-11-29 18:37:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 )
 
 # --- JWT settings ---
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6A7B8C9D0E1F2")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
